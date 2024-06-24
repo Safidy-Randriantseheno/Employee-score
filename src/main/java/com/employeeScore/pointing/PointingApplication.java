454898998aa54699260dda9f9d03732dd60fbede
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-06-24 09:50:41 +0300

    feat: initial project .

---
+++ src/main/java/com/employeeScore/pointing/PointingApplication.java
@@ -0,0 +1,13 @@
+package com.employeeScore.pointing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeeScore/pointing/PointingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeScore.pointing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
